5bfe2ba9ed725f26c5c17185c040e9016100f946
Shashank Chandraksha Bangera <shashank.c.bangera@fau.de>
2026-01-19 00:43:43 +0100

    Delete random_agent.py

--- fauhalma/agents/random_agent.py
+++
@@ -1,7 +0,0 @@
-import random
-from fauhalma.moves import legal_moves
-from fauhalma.state import State
-
-def choose_move(state: State, shape: str):
-    moves = legal_moves(state, "A", shape)
-    return random.choice(moves)
